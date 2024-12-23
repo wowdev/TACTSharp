b99b46ccf08efa93f0917ee351fbfa396f2bd64f
Martin Benjamins <ictmartin@gmail.com>
2024-12-23 07:30:50 +0100

    Initial port

---
+++ IndexInstance.cs
@@ -0,0 +1,133 @@
+﻿using System.IO.MemoryMappedFiles;
+
+namespace TACTIndexTestCSharp
+{
+    public sealed class IndexInstance
+    {
+        private long indexSize;
+        private IndexFooter footer;
+        private short archiveIndex = -1;
+        private string path;
+        private bool isGroupArchive;
+        private readonly List<byte[]> lastEKeys = [];
+
+        public IndexInstance(string path, short archiveIndex = -1)
+        {
+            this.archiveIndex = archiveIndex;
+            this.path = path;
+            indexSize = new FileInfo(path).Length;
+
+            using (var indexFile = MemoryMappedFile.CreateFromFile(path, FileMode.Open, Path.GetFileNameWithoutExtension(path)))
+            {
+                using (var accessor = indexFile.CreateViewAccessor(indexSize - 20, 20, MemoryMappedFileAccess.Read))
+                    accessor.Read(0, out footer);
+
+                isGroupArchive = footer.offsetBytes == 6;
+
+                var blockSizeBytes = footer.blockSizeKBytes << 10;
+                var entrySize = footer.keyBytes + footer.sizeBytes + footer.offsetBytes;
+                var entriesPerBlock = blockSizeBytes / entrySize;
+                var numBlocks = (int)Math.Ceiling((double)footer.numElements / entriesPerBlock);
+
+                var ofsStartOfToc = numBlocks * blockSizeBytes;
+                var ofsEndOfTocEkeys = ofsStartOfToc + footer.keyBytes * numBlocks;
+
+                var data = new byte[ofsEndOfTocEkeys - ofsStartOfToc];
+
+                using (var accessor = indexFile.CreateViewAccessor(ofsStartOfToc, ofsEndOfTocEkeys - ofsStartOfToc, MemoryMappedFileAccess.Read))
+                    accessor.ReadArray(0, data, 0, data.Length);
+
+                var dataAsSpan = data.AsSpan();
+
+                for (var i = 0; i < numBlocks; i++)
+                {
+                    var eKeyCompare = dataAsSpan.Slice(i * footer.keyBytes, footer.keyBytes);
+                    lastEKeys.Add(eKeyCompare.ToArray());
+                }
+            }
+        }
+
+        public (int offset, int size, int archiveIndex) GetIndexInfo(string targetEKey)
+        {
+            var eKeyTarget = Convert.FromHexString(targetEKey).AsSpan();
+            var targetBlock = 0;
+
+            foreach (var lastEKey in lastEKeys)
+            {
+                if (lastEKey.AsSpan().SequenceCompareTo(eKeyTarget) > 0)
+                    break;
+
+                targetBlock++;
+            }
+
+            var blockIndexMaybeContainingEkey = targetBlock;
+
+            var blockSizeBytes = footer.blockSizeKBytes << 10;
+            var entrySize = footer.keyBytes + footer.sizeBytes + footer.offsetBytes;
+            var entriesPerBlock = blockSizeBytes / entrySize;
+            var numBlocks = (int)Math.Ceiling((double)footer.numElements / entriesPerBlock);
+            var ofsStartOfCandidateBlock = blockIndexMaybeContainingEkey * blockSizeBytes;
+            var entriesOfCandidateBlock = blockIndexMaybeContainingEkey != numBlocks - 1 ? entriesPerBlock : (footer.numElements - (numBlocks - 1) * entriesPerBlock);
+            var ofsEndOfCandidateBlock = ofsStartOfCandidateBlock + entrySize * entriesOfCandidateBlock;
+
+            if (ofsEndOfCandidateBlock >= indexSize)
+            {
+                return (-1, -1, -1);
+            }
+
+            byte[] candidateBlockData = new byte[ofsEndOfCandidateBlock - ofsStartOfCandidateBlock];
+
+            using (var indexFile = MemoryMappedFile.CreateFromFile(path, FileMode.Open, Path.GetFileNameWithoutExtension(path), 0, MemoryMappedFileAccess.Read))
+            {
+                using (var accessor = indexFile.CreateViewAccessor(ofsStartOfCandidateBlock, candidateBlockData.Length, MemoryMappedFileAccess.Read))
+                {
+                    accessor.ReadArray(0, candidateBlockData, 0, candidateBlockData.Length);
+                }
+            }
+            //using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read))
+            //{
+            //    fs.Position = ofsStartOfCandidateBlock;
+            //    fs.ReadExactly(candidateBlockData, 0, (int)ofsEndOfCandidateBlock - ofsStartOfCandidateBlock);
+            //}
+
+            var candidateBlockDataAsSpan = candidateBlockData.AsSpan();
+
+            for (int i = 0; i < entriesOfCandidateBlock; i++)
+            {
+                var entry = candidateBlockDataAsSpan.Slice(i * entrySize, entrySize);
+                if (entry.Slice(0, footer.keyBytes).SequenceEqual(eKeyTarget))
+                {
+                    if (isGroupArchive)
+                    {
+                        var encodedSize = entry.Slice(footer.keyBytes, footer.sizeBytes).ReadInt32BE();
+                        var fileArchiveIndex = entry.Slice(footer.keyBytes + footer.sizeBytes, 2).ReadInt16BE();
+                        var offset = entry.Slice(footer.keyBytes + footer.sizeBytes + 2, 4).ReadInt32BE();
+                        return (offset, encodedSize, fileArchiveIndex);
+                    }
+                    else
+                    {
+                        var encodedSize = entry.Slice(footer.keyBytes, footer.sizeBytes).ReadInt32BE();
+                        var offset = entry.Slice(footer.keyBytes + footer.sizeBytes, footer.offsetBytes).ReadInt32BE();
+                        return (offset, encodedSize, archiveIndex);
+                    }
+                }
+            }
+
+            return (-1, -1, -1);
+        }
+
+        private unsafe struct IndexFooter
+        {
+            public byte formatRevision;
+            public byte flags0;
+            public byte flags1;
+            public byte blockSizeKBytes;
+            public byte offsetBytes;
+            public byte sizeBytes;
+            public byte keyBytes;
+            public byte hashBytes;
+            public uint numElements;
+            public fixed byte bytefooterHash[8];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,202 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Linq.Expressions;
+
+namespace TACTIndexTestCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // mostly based on schlumpf's implementation, but with some changes because i dont know how to port some c++ things to c# properly
+
+            var baseDir = "C:\\World of Warcraft\\data\\";
+
+            var inputArchive = Path.Combine(baseDir, "indices\\ec39d1815cbe1434aa9c77db96ab4211.index");
+            var cdnConfig = Path.Combine(baseDir, "config\\cd\\18\\cd18191b8928c33bf24b962e9330460f");
+
+            IndexInstance groupIndex;
+            var indices = new List<IndexInstance>();
+
+            var groupArchiveIndex = "";
+            var archives = new List<string>();
+
+            foreach (var line in File.ReadAllLines(cdnConfig))
+            {
+                var parts = line.Split(" = ");
+                if (parts[0] == "archive-group")
+                    groupArchiveIndex = parts[1];
+                else if (parts[0] == "archives")
+                    archives.AddRange(parts[1].Split(" "));
+            }
+
+            var gaSW = new Stopwatch();
+            var archiveSW = new Stopwatch();
+
+            if (!File.Exists(Path.Combine(baseDir, "indices", groupArchiveIndex + ".index")))
+            {
+                // TODO: Make it? 
+                throw new Exception("Group archive index not found");
+            }
+
+            gaSW.Start();
+            groupIndex = new IndexInstance(Path.Combine(baseDir, "indices", groupArchiveIndex + ".index"));
+            gaSW.Stop();
+            Console.WriteLine("Loaded group archive index in " + gaSW.Elapsed.TotalMilliseconds + "ms");
+
+            var ramAfterGroupIndex = Process.GetCurrentProcess().WorkingSet64;
+            Console.WriteLine("RAM usage after group index: " + ramAfterGroupIndex / 1024 / 1024 + "MB");
+
+            archiveSW.Start();
+            for (var i = 0; i < archives.Count; i++)
+            {
+                var archive = archives[i];
+                if (File.Exists(Path.Combine(baseDir, "indices", archive + ".index")))
+                {
+                    indices.Add(new IndexInstance(Path.Combine(baseDir, "indices", archive + ".index"), (short)i));
+                }
+            }
+            archiveSW.Stop();
+            Console.WriteLine("Loaded " + indices.Count + " indices in " + archiveSW.Elapsed.TotalMilliseconds + "ms");
+            var ramDiffAfterLoose = Process.GetCurrentProcess().WorkingSet64 - ramAfterGroupIndex;
+            Console.WriteLine("RAM usage difference loose indices: " + ramDiffAfterLoose / 1024 / 1024 + "MB");
+
+            var oldIndexSW = new Stopwatch();
+            oldIndexSW.Start();
+            GetIndexes(baseDir, archives.ToArray());
+            oldIndexSW.Stop();
+            Console.WriteLine("Loaded " + indexDictionary.Count + " indices in " + oldIndexSW.Elapsed.TotalMilliseconds + "ms");
+            var ramDiffAfterOld = Process.GetCurrentProcess().WorkingSet64 - ramAfterGroupIndex - ramAfterGroupIndex;
+            Console.WriteLine("RAM usage difference old indices: " + ramDiffAfterOld / 1024 / 1024 + "MB");
+
+            var checkedKeys = 0;
+            var looseFaster = 0;
+            var groupFaster = 0;
+
+            foreach (var line in File.ReadAllLines("D:\\Downloads\\11.1.encodingdump")) // https://old.wow.tools/pub/11.1.encodingdump
+            {
+                var parts = line.Split(' ');
+
+                gaSW.Restart();
+                var (gaOffset, gaSize, gaArchiveIndex) = groupIndex.GetIndexInfo(parts[1]);
+                gaSW.Stop();
+
+                if (gaOffset != -1)
+                    Console.WriteLine("Group (" + gaOffset + ", " + gaSize + ", " + gaArchiveIndex + " lookup took " + gaSW.Elapsed.TotalMilliseconds + "ms");
+
+                var found = false;
+
+                archiveSW.Restart();
+
+                Parallel.ForEach(indices, index =>
+                {
+                    if(found)
+                        return;
+                    var (offset, size, archiveIndex) = index.GetIndexInfo(parts[1]);
+                    if (offset != -1)
+                    {
+                        archiveSW.Stop();
+                        found = true;
+                        Console.WriteLine("Loose (" + offset + ", " + size + ", " + archiveIndex + " lookup took " + archiveSW.Elapsed.TotalMilliseconds + "ms");
+                    }
+                });
+
+                oldIndexSW.Restart();
+                if (indexDictionary.TryGetValue(parts[1].ToUpper(), out var indexEntry))
+                {
+                    oldIndexSW.Stop();
+                    Console.WriteLine("Old (" + indexEntry.offset + ", " + indexEntry.size + ", " + indexEntry.index + " lookup took " + oldIndexSW.Elapsed.TotalMilliseconds + "ms");
+                }
+                oldIndexSW.Stop();
+
+                if (gaSW.Elapsed.TotalMilliseconds > archiveSW.Elapsed.TotalMilliseconds)
+                {
+                    looseFaster++;
+                }
+                else
+                {
+                    groupFaster++;
+                }
+
+                checkedKeys++;
+
+                if(checkedKeys % 1000 == 0)
+                {
+                    Console.WriteLine("Checked " + checkedKeys + " keys");
+                    Console.WriteLine("Group faster: " + groupFaster);
+                    Console.WriteLine("Loose faster: " + looseFaster);
+                }
+            }
+        }
+
+        public struct IndexEntry
+        {
+            public short index;
+            public uint offset;
+            public uint size;
+        }
+
+        private static ReaderWriterLockSlim cacheLock = new ReaderWriterLockSlim();
+        private static Dictionary<string, IndexEntry> indexDictionary = new Dictionary<string, IndexEntry>();
+
+        private static void GetIndexes(string basePath, string[] archives)
+        {
+            Parallel.ForEach(archives, (archive, state, i) =>
+            {
+                try
+                {
+                    byte[] indexContent = File.ReadAllBytes(Path.Combine(basePath, "indices", archive + ".index"));
+
+                    using (BinaryReader bin = new BinaryReader(new MemoryStream(indexContent)))
+                    {
+                        int indexEntries = indexContent.Length / 4096;
+
+                        for (var b = 0; b < indexEntries; b++)
+                        {
+                            for (var bi = 0; bi < 170; bi++)
+                            {
+                                var headerHash = Convert.ToHexString(bin.ReadBytes(16));
+
+                                var entry = new IndexEntry()
+                                {
+                                    index = (short)i,
+                                    size = bin.ReadUInt32(true),
+                                    offset = bin.ReadUInt32(true)
+                                };
+
+                                cacheLock.EnterUpgradeableReadLock();
+                                try
+                                {
+                                    if (!indexDictionary.ContainsKey(headerHash))
+                                    {
+                                        cacheLock.EnterWriteLock();
+                                        try
+                                        {
+                                            if (!indexDictionary.TryAdd(headerHash, entry))
+                                            {
+                                                Console.WriteLine("Duplicate index entry for " + headerHash + " " + "(index: " + archives[i] + ", size: " + entry.size + ", offset: " + entry.offset);
+                                            }
+                                        }
+                                        finally
+                                        {
+                                            cacheLock.ExitWriteLock();
+                                        }
+                                    }
+                                }
+                                finally
+                                {
+                                    cacheLock.ExitUpgradeableReadLock();
+                                }
+                            }
+                            bin.ReadBytes(16);
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Error retrieving index: " + e.Message);
+                }
+            });
+        }
+    }
+}
