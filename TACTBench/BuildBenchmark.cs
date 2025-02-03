fbbb631f252b8f14d193f77e0be6f1e8116d17fb
Martin Benjamins <ictmartin@gmail.com>
2025-02-03 04:15:22 +0100

    Add benchmark project
    Switched to only loading builds for now
    
    Co-Authored-By: Warpten <563936+Warpten@users.noreply.github.com>

---
+++ TACTBench/BuildBenchmark.cs
@@ -0,0 +1,42 @@
+﻿using BenchmarkDotNet.Attributes;
+
+using TACTSharp;
+
+namespace TACTBench
+{
+    [MemoryDiagnoser]
+    public class BuildBenchmark
+    {
+
+        [GlobalSetup]
+        public async Task EnsureBuildDownloaded()
+        {
+            await LoadBuild();
+        }
+
+        public async Task LoadBuild()
+        {
+            var versions = await CDN.GetProductVersions("wow");
+            foreach (var line in versions.Split('\n'))
+            {
+                if (!line.StartsWith("us|"))
+                    continue;
+
+                var splitLine = line.Split('|');
+
+                Settings.BuildConfig ??= splitLine[1];
+                Settings.CDNConfig ??= splitLine[2];
+                break;
+            }
+
+            var _build = new BuildInstance(Settings.BuildConfig!, Settings.CDNConfig!);
+            await _build.Load();
+        }
+
+        [Benchmark]
+        public async Task TestBuildLoad()
+        {
+            await LoadBuild();
+        }
+    }
+}
